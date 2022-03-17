1b376fedade2ea7d55de6a1f49867209679d1806
Suyannesara <suyannesara17@gmail.com>
2022-03-17 09:34:51 -0300

    Learning abour arrays

---
+++ AulaJS/arrays.js
@@ -0,0 +1,11 @@
+let lsCar = ["Ferrari", "Porshe", "Lamboorguini"];
+
+document.write(`<br>O carro que está no índice ${i} = ${lsCar[i]}`)
+
+
+document.write(`<br>O primeiro carro é ${0}</br>`)
+document.write(`<br>O último carro é ${lsCar[lsCar.length - 1]}</br>`)
+
+
+let texto = ""
+
