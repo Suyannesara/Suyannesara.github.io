e3062f5ec990b0e9aade2f8e73079471ff0f03ab
Suyannesara <suyannesara17@gmail.com>
2022-03-21 09:11:48 -0300

    Trying to make challenge

---
+++ ATV9/q6.js
@@ -0,0 +1,21 @@
+function jogar(){
+  //Array
+  options = ["pedra", "papel", "tesoura"];
+
+  sorteado = Math.floor(Math.random(options));
+  alert(sorteado)
+  //document.getElementsByClassName("adversario").value =  ;
+
+
+}
+
+
+// if (id == sorteio){
+
+// }
+
+// if (sorteio == pedra)
+
+// tesoura -> papel
+// papel -> pedra
+// pedra -> tesoura
